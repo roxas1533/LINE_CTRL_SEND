14afce019573d536cc0f7762731c9fbdabec18c4
roxas1533 <roxas1533@gmail.com>
2020-11-06 19:37:41 +0900

    負荷軽減及びReleaseビルド

--- LINE_CTRL/main.cpp
+++ LINE_CTRL/main.cpp
@@ -3,56 +3,73 @@
 
 bool ctFlag = false;
 bool enFlag = false;
-TCHAR className[1024];
-VOID CALLBACK TimerProc(HWND hWnd, UINT uMsg, UINT idEvent, DWORD dwTime) {
+bool isLine = false;
+const TCHAR p[] = TEXT("Qt5QWindowIcon");
 
-		HWND hwnd = GetForegroundWindow();
-		GetClassName(hwnd, className, 1024);
-		TCHAR p[] = TEXT("Qt5QWindowIcon");
-		if (!_tcscmp(p, className)) {
-			if (GetAsyncKeyState(VK_CONTROL) & 0x8000&&!enFlag) {
-				ctFlag = true;
-			}
-			if (GetAsyncKeyState(VK_RETURN) & 0x8000 && !ctFlag) {
-				enFlag = true;
-			}
-			if (ctFlag&&GetAsyncKeyState(VK_RETURN) & 0x8000) {
-				INPUT inp[1];
-				inp[0].type = INPUT_KEYBOARD;
-				inp[0].ki.wVk = VK_MENU;
-				inp[0].ki.wScan = (short)MapVirtualKey(VK_MENU, 0);
-				inp[0].ki.dwFlags = KEYEVENTF_EXTENDEDKEY;
-				inp[0].ki.dwExtraInfo = 0;
-				inp[0].ki.time = 0;
-				SendInput(1, inp, sizeof(INPUT));
-				inp[0].type = INPUT_KEYBOARD;
-				inp[0].ki.wVk = VK_RETURN; 
-				inp[0].ki.wScan = (short)MapVirtualKey(VK_RETURN, 0);
-				inp[0].ki.dwFlags = KEYEVENTF_EXTENDEDKEY;
-				inp[0].ki.dwExtraInfo = 0;
-				inp[0].ki.time = 0;
-				SendInput(1, inp, sizeof(INPUT));
-				inp[0].type = INPUT_KEYBOARD;
-				inp[0].ki.wVk = VK_MENU;
-				inp[0].ki.wScan = (short)MapVirtualKey(VK_MENU, 0);
-				inp[0].ki.dwFlags = KEYEVENTF_EXTENDEDKEY|KEYEVENTF_KEYUP;
-				inp[0].ki.dwExtraInfo = 0;
-				inp[0].ki.time = 0;
-				SendInput(1, inp, sizeof(INPUT));
-				inp[0].type = INPUT_KEYBOARD;
-				inp[0].ki.wVk = VK_RETURN;
-				inp[0].ki.wScan = (short)MapVirtualKey(VK_RETURN, 0);
-				inp[0].ki.dwFlags = KEYEVENTF_EXTENDEDKEY|KEYEVENTF_KEYUP;
-				inp[0].ki.dwExtraInfo = 0;
-				inp[0].ki.time = 0;
-				SendInput(1, inp, sizeof(INPUT));
-			}
-			if (!(GetAsyncKeyState(VK_RETURN) & 0x8000)) {
-				enFlag = false;
-			}
-			if (!(GetAsyncKeyState(VK_CONTROL) & 0x8000)) {
-				ctFlag = false;
-			}
+TCHAR className[1024];
+VOID CALLBACK LineTimer(HWND hWnd, UINT uMsg, UINT idEvent, DWORD dwTime);
+VOID CALLBACK TimerProc(HWND hWnd, UINT uMsg, UINT idEvent, DWORD dwTime) {
+	HWND hwnd = GetForegroundWindow();
+	GetClassName(hwnd, className, 1024);
+
+	if (!_tcscmp(p, className)) {
+		isLine = true;
+		KillTimer(hWnd, 1);
+		SetTimer(hWnd, 2, 10, (TIMERPROC)LineTimer);
+	}
+}
+
+VOID CALLBACK LineTimer(HWND hWnd, UINT uMsg, UINT idEvent, DWORD dwTime) {
+	HWND hwnd = GetForegroundWindow();
+	GetClassName(hwnd, className, 1024);
+
+	if (_tcscmp(p, className)) {
+		isLine = false;
+		KillTimer(hWnd, 2);
+		SetTimer(hWnd, 1, 100, (TIMERPROC)TimerProc);
+	}
+	if (GetAsyncKeyState(VK_CONTROL) & 0x8000 && !enFlag) {
+		ctFlag = true;
+	}
+	if (GetAsyncKeyState(VK_RETURN) & 0x8000 && !ctFlag) {
+		enFlag = true;
+	}
+	if (ctFlag && GetAsyncKeyState(VK_RETURN) & 0x8000) {
+		INPUT inp[1];
+		inp[0].type = INPUT_KEYBOARD;
+		inp[0].ki.wVk = VK_MENU;
+		inp[0].ki.wScan = (short)MapVirtualKey(VK_MENU, 0);
+		inp[0].ki.dwFlags = KEYEVENTF_EXTENDEDKEY;
+		inp[0].ki.dwExtraInfo = 0;
+		inp[0].ki.time = 0;
+		SendInput(1, inp, sizeof(INPUT));
+		inp[0].type = INPUT_KEYBOARD;
+		inp[0].ki.wVk = VK_RETURN;
+		inp[0].ki.wScan = (short)MapVirtualKey(VK_RETURN, 0);
+		inp[0].ki.dwFlags = KEYEVENTF_EXTENDEDKEY;
+		inp[0].ki.dwExtraInfo = 0;
+		inp[0].ki.time = 0;
+		SendInput(1, inp, sizeof(INPUT));
+		inp[0].type = INPUT_KEYBOARD;
+		inp[0].ki.wVk = VK_MENU;
+		inp[0].ki.wScan = (short)MapVirtualKey(VK_MENU, 0);
+		inp[0].ki.dwFlags = KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP;
+		inp[0].ki.dwExtraInfo = 0;
+		inp[0].ki.time = 0;
+		SendInput(1, inp, sizeof(INPUT));
+		inp[0].type = INPUT_KEYBOARD;
+		inp[0].ki.wVk = VK_RETURN;
+		inp[0].ki.wScan = (short)MapVirtualKey(VK_RETURN, 0);
+		inp[0].ki.dwFlags = KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP;
+		inp[0].ki.dwExtraInfo = 0;
+		inp[0].ki.time = 0;
+		SendInput(1, inp, sizeof(INPUT));
+	}
+	if (!(GetAsyncKeyState(VK_RETURN) & 0x8000)) {
+		enFlag = false;
+	}
+	if (!(GetAsyncKeyState(VK_CONTROL) & 0x8000)) {
+		ctFlag = false;
 	}
 }
 
@@ -62,7 +79,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
 		PostQuitMessage(0);
 		return 0;
 	case WM_CREATE:
-		SetTimer(hwnd, 1, 10, (TIMERPROC)TimerProc);
+		SetTimer(hwnd, 1, 100, (TIMERPROC)TimerProc);
 		return 0;
 	}
 	return DefWindowProc(hwnd, msg, wp, lp);
@@ -87,13 +104,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	if (!RegisterClass(&winc)) return 0;
 	HANDLE hMSP;
 
-
 	hMSP = CreateMutex(NULL, TRUE, L"LINE-CTRL");
 	if (GetLastError() == ERROR_ALREADY_EXISTS) {
 		MessageBox(NULL, L"ëΩèdãNìÆ"
 			, L"LINE-CTRL", MB_OK);
-		ReleaseMutex(hMSP);
-		CloseHandle(hMSP);
+		if (hMSP) {
+			ReleaseMutex(hMSP);
+			CloseHandle(hMSP);
+		}
 		return FALSE;
 	}
 	hwnd = CreateWindow(
